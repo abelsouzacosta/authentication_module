9714058a2494d7cd9e4682c7a267bc354aadf0cc
Abel Souza <abelsouzacosta@gmail.com>
2021-04-13 03:00:50 -0300

    fix: not used object was removed

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
 import CreateSessionService from '../services/CreateSessionService';
 
 export default class SessionsController {
