743fdc16143c7bfcf617ee695ce3332a02ed9d39
Abel Souza <abelsouzacosta@gmail.com>
2021-04-13 00:02:37 -0300

    feat(errors): dealing with application errors
    Feature is a middleawre to interecept all application errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,34 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
+import AppError from '@shared/errors/AppError';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   return res.status(200).send({
     message: 'Bem vindo',
+  });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return res.status(400).json({
+    status: 'error',
+    message: 'Internal server error',
   });
 });
 
